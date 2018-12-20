83a4539086da516b66d623bbb464b4d379c6f45f
Isaias Pomales <impomales@gmail.com>
2018-12-20 14:24:53 -0500

    add button layout of calculator

--- client/components/calculator.js
+++ client/components/calculator.js
@@ -2,7 +2,45 @@ import React, {Component} from 'react'
 
 class Calculator extends Component {
   render() {
-    return <h1>Calculator</h1>
+    return (
+      <div>
+        <div className="calculator">
+          <div className="display" />
+
+          <div className="buttons">
+            <table>
+              <tr>
+                <td><button>7</button></td>
+                <td><button>8</button></td>
+                <td><button>9</button></td>
+                <td><button>+</button></td>
+                <td><button>-</button></td>
+              </tr>
+              <tr>
+                <td><button>4</button></td>
+                <td><button>5</button></td>
+                <td><button>6</button></td>
+                <td><button>*</button></td>
+                <td><button>/</button></td>
+              </tr>
+              <tr>
+                <td><button>3</button></td>
+                <td><button>2</button></td>
+                <td><button>1</button></td>
+                <td><button>.</button></td>
+                <td><button>=</button></td>
+              </tr>
+              <tr>
+                <td><button>0</button></td>
+              </tr>
+            </table>
+          </div>
+
+          <div className="output" />
+        </div>
+        <div className="labels" />
+      </div>
+    )
   }
 }
 
