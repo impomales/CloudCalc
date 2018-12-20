1ea82dc9c0c71ea51f3fcf156f393aaaca7d4f81
Isaias Pomales <impomales@gmail.com>
2018-12-20 14:29:09 -0500

    define state for calc component, add type label to buttons

--- client/components/calculator.js
+++ client/components/calculator.js
@@ -1,6 +1,17 @@
 import React, {Component} from 'react'
 
 class Calculator extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      firstNumber: 0,
+      secondNumber: 0,
+      result: 0,
+      operation: '',
+      title: ''
+    }
+  }
   render() {
     return (
       <div>
@@ -10,33 +21,68 @@ class Calculator extends Component {
           <div className="buttons">
             <table>
               <tr>
-                <td><button>7</button></td>
-                <td><button>8</button></td>
-                <td><button>9</button></td>
-                <td><button>+</button></td>
-                <td><button>-</button></td>
+                <td>
+                  <button type="button">7</button>
+                </td>
+                <td>
+                  <button type="button">8</button>
+                </td>
+                <td>
+                  <button type="button">9</button>
+                </td>
+                <td>
+                  <button type="button">+</button>
+                </td>
+                <td>
+                  <button type="button">-</button>
+                </td>
               </tr>
               <tr>
-                <td><button>4</button></td>
-                <td><button>5</button></td>
-                <td><button>6</button></td>
-                <td><button>*</button></td>
-                <td><button>/</button></td>
+                <td>
+                  <button type="button">4</button>
+                </td>
+                <td>
+                  <button type="button">5</button>
+                </td>
+                <td>
+                  <button type="button">6</button>
+                </td>
+                <td>
+                  <button type="button">*</button>
+                </td>
+                <td>
+                  <button type="button">/</button>
+                </td>
               </tr>
               <tr>
-                <td><button>3</button></td>
-                <td><button>2</button></td>
-                <td><button>1</button></td>
-                <td><button>.</button></td>
-                <td><button>=</button></td>
+                <td>
+                  <button type="button">3</button>
+                </td>
+                <td>
+                  <button type="button">2</button>
+                </td>
+                <td>
+                  <button type="button">1</button>
+                </td>
+                <td>
+                  <button type="button">.</button>
+                </td>
+                <td>
+                  <button type="button">=</button>
+                </td>
               </tr>
               <tr>
-                <td><button>0</button></td>
+                <td>
+                  <button type="button">0</button>
+                </td>
               </tr>
             </table>
           </div>
 
-          <div className="output" />
+          <div>
+            <div className="output" />
+            <button type="submit">Save</button>
+          </div>
         </div>
         <div className="labels" />
       </div>
